c6596bfcddec45a0ef93dec730a97ffdbdcf11e5
jnelson <61723804+jnel-221@users.noreply.github.com>
2020-12-20 15:44:46 -0600

    created variables to store user input

--- script.js
+++ script.js
@@ -3,14 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //variables to get password length from user and change user input from string to number
   var length = prompt("Please select a password length between 8 and 128 characters");
   var numLength =parseInt(length);
-  // console.log(numLength);
+
+  //variables to hold user-selected password criteria
+  var upperCase = confirm("Do you want uppercase letters?");
+  var lowerCase = confirm("Do you want lowercase letters?");
+  var nums =confirm("Do you want numbers?");
+  var speChar = confirm("Do you want special characters?");
+ console.log(upperCase);
+ console.log(lowerCase);
+ console.log(nums);
+ console.log(speChar);
   if (numLength >=8 && numLength <= 128) {
     // console.log(numLength);
     // console.log(numLength >= 8);
     // console.log(numLength <= 128);
-    alert("thanks for following directions");
+    alert("Please select from the following criteria: ");
   } else {
     // console.log(numLength);
     // console.log(numLength >= 8);
