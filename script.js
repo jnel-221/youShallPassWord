09c276f7b2fd5ec28cd94ddc94509dd6a6590f6a
jnelson <61723804+jnel-221@users.noreply.github.com>
2020-12-20 16:00:26 -0600

    declared user criteria variables at top of page, moved assigned variables into first conditional statement

--- script.js
+++ script.js
@@ -3,31 +3,37 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  //variables to get password length from user and change user input from string to number
+  //variables to get password length from user and conditions to validate correct length
+  //browser to refresh if incorrect length chosen
   var length = prompt("Please select a password length between 8 and 128 characters");
   var numLength =parseInt(length);
 
-  //variables to hold user-selected password criteria
+  //declare variables for password criteria outside of conditional statement:
+  var upperCase;
+  var lowerCase;
+  var nums;
+  var speChar;
+
+  if (numLength >=8 && numLength <= 128) {
+   
+    alert("Please select from the following criteria: ");
+     //variables to hold user-selected password criteria
   var upperCase = confirm("Do you want uppercase letters?");
   var lowerCase = confirm("Do you want lowercase letters?");
   var nums =confirm("Do you want numbers?");
   var speChar = confirm("Do you want special characters?");
- console.log(upperCase);
- console.log(lowerCase);
- console.log(nums);
- console.log(speChar);
-  if (numLength >=8 && numLength <= 128) {
-    // console.log(numLength);
-    // console.log(numLength >= 8);
-    // console.log(numLength <= 128);
-    alert("Please select from the following criteria: ");
+  console.log(upperCase);
+  console.log(lowerCase);
+  console.log(nums);
+  console.log(speChar);
   } else {
-    // console.log(numLength);
-    // console.log(numLength >= 8);
-    // console.log(numLength <= 128);
+   
     alert("Please enter a numeric value that is between 8 and 128 characters long");
     location.reload();
   }
+
+ 
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
