2a01922c9ed8d0b9746be95fea95c4205f4ef0cd
jnelson <61723804+jnel-221@users.noreply.github.com>
2020-12-20 17:47:34 -0600

    moved all variable declarations outside of writePassword function

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var promptLength = ""
+var numLength;
+
+var upperCase;
+var lowerCase;
+var nums;
+var speChar;
 
 // Write password to the #password input
 function writePassword() {
   //variables to get password length from user and conditions to validate correct length
   //browser to refresh if incorrect length chosen
   promptLength = prompt("Please select a password length between 8 and 128 characters");
-  var numLength =parseInt(promptLength);
+  numLength =parseInt(promptLength);
 
   //declare variables for password criteria outside of conditional statement:
-  var upperCase;
-  var lowerCase;
-  var nums;
-  var speChar;
+  
 
   if (numLength >=8 && numLength <= 128) {
    
     alert("Please select password criteria from the following pop-up windows.");
      //run password criteria confirms if password length meets criteria, otherwise run else statement only
-  var upperCase = confirm("Do you want uppercase letters?");
-  var lowerCase = confirm("Do you want lowercase letters?");
-  var nums =confirm("Do you want numbers?");
-  var speChar = confirm("Do you want special characters?");
+   upperCase = confirm("Do you want uppercase letters?");
+   lowerCase = confirm("Do you want lowercase letters?");
+   nums =confirm("Do you want numbers?");
+   speChar = confirm("Do you want special characters?");
   
   while (upperCase !== true && lowerCase !== true && nums !== true && speChar!== true) {
     alert("Please select password criteria from the following pop-up windows.");
      //run password criteria confirms if password length meets criteria, otherwise run else statement only
-  var upperCase = confirm("Do you want uppercase letters?");
-  var lowerCase = confirm("Do you want lowercase letters?");
-  var nums =confirm("Do you want numbers?");
-  var speChar = confirm("Do you want special characters?");
+   upperCase = confirm("Do you want uppercase letters?");
+   lowerCase = confirm("Do you want lowercase letters?");
+   nums =confirm("Do you want numbers?");
+   speChar = confirm("Do you want special characters?");
    console.log(upperCase);
   console.log(lowerCase);
   console.log(nums);
